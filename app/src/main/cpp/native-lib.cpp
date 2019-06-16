2c6157628a972a9fab6e6fe99e1a1c353bfb02d1
Helias <stefanoborzi32@gmail.com>
2019-06-16 12:14:23 +0200

    Import project

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,17 @@
+/*
+#include
+#include
+#include
+#include
+#include
+ 
+extern "C"
+jstring
+Java_com_example_helias_myprojectopencv_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */ /*) {
+
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+*/
---
+++ app/src/main/java/com/example/myprojopencv/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myprojopencv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
+
+        // Button to call OpenCV Camera Activity
+        Button cameraInit = (Button) findViewById(R.id.cameraInit);
+        cameraInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),OpencvCamera.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
